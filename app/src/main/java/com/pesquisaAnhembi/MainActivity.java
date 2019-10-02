188bded5bd31a1ca4e85df0ef4bc1ec68a0dce84
ElvisBM <elvisbmartins@gmail.com>
2019-10-02 13:14:33 -0300

    criado activitys instrucoes e pesquisa

--- app/src/main/java/com/pesquisaAnhembi/MainActivity.java
+++ app/src/main/java/com/pesquisaAnhembi/MainActivity.java
@@ -3,6 +3,9 @@ package com.pesquisaAnhembi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonEntra = findViewById(R.id.buttonEntrar);
+        buttonEntra.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //System.out.println("Button Clicked");
+
+                Intent activityInstrucoes = new Intent(getApplicationContext(), instrucoes.class);
+                startActivity(activityInstrucoes);
+            }
+        });
     }
 }
--- app/src/main/java/com/pesquisaAnhembi/instrucoes.java
+++ app/src/main/java/com/pesquisaAnhembi/instrucoes.java
@@ -2,7 +2,10 @@ package com.pesquisaAnhembi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class instrucoes extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class instrucoes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrucoes);
+        Button buttonIniciar = findViewById(R.id.buttonIniciar);
+        buttonIniciar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //System.out.println("Button Clicked");
+
+                Intent activityPesquisa = new Intent(getApplicationContext(), pesquisa.class);
+                startActivity(activityPesquisa);
+            }
+        });
     }
 }
