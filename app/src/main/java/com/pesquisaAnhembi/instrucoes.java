60039d4b19da903563f1464414ef4e9795f8af99
ElvisBM <elvisbmartins@gmail.com>
2019-10-02 13:13:31 -0300

    .

---
+++ app/src/main/java/com/pesquisaAnhembi/instrucoes.java
@@ -0,0 +1,14 @@
+package com.pesquisaAnhembi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class instrucoes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instrucoes);
+    }
+}
